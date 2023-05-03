0b13d4a7dbbb200a594ae983728789f098638f40
Tim Hofmann <Tim.Hofmann@students.bs-linz2.ac.at>
2023-05-03 15:36:48 +0200

    removed ejs

--- code/Backend/server.js
+++ code/Backend/server.js
@@ -1,23 +1,13 @@
 //TODO: convert to ES modules
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const app = express();
-
-const publicDir = path.join(__dirname, 'public');
-const viewsDir = path.join(__dirname, 'views');
-
-app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
 	res.redirect('/api');
-});
-
-app.route('/api').get(function (req, res) {
-	res.render('index');
 });
 
 app.use('/api/articles', require('./routes/articles'));
