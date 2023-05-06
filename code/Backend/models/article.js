7c12b86d1df780a8d7be9891d8bc3c50dd3734e3
Tim <timhofmann01@gmail.com>
2023-05-06 16:50:17 +0200

    fixed and finished article routes

--- code/Backend/models/article.js
+++ code/Backend/models/article.js
@@ -10,26 +10,40 @@ module.exports = class Article {
 	}
 
 	/**
-	 * Finds all articles.
+	 * Lists all articles.
 	 * @returns {Promise<Array<Article>>}
 	 */
-	static async find() {
+	static async list() {
 		const [rows] = await db.query('SELECT * FROM articles');
-		return rows.map((row) => new Article(row.id, row.name, row.description, row.seller));
+		return rows.map((row) => new Article(row.id, row.name, row.description, row.price, row.seller));
 	}
 
 	/**
-	 * Find an article by id.
-	 * @returns {Promise<Article>}
-	 */
-
-	/**
-	 * Saves an article to the database.
+	 * Finds an article by id.
 	 * @returns {Promise<Article>}
 	 */
 	static async findById(id) {
 		const [rows] = await db.query('SELECT * FROM articles WHERE id = ?', [id]);
-		return new Article(rows[0].id, rows[0].name, rows[0].description, rows[0].seller);
+		return new Article(rows[0].id, rows[0].name, rows[0].description, rows[0].price, rows[0].seller);
+	}
+
+	/**
+	 * Gets the next id for a new article.
+	 * @returns {number}
+	 */
+	static async nextId() {
+		const [rows] = await db.query('SELECT MAX(id) AS max_id FROM articles');
+		return rows[0].max_id + 1;
+	}
+
+	/**
+	 * Saves an article in the database.
+	 * @returns {Promise<Article>}
+	 */
+	async save() {
+		const [result] = await db.query('INSERT INTO articles (name, description, price, seller_id) VALUES (?, ?, ?, ?)', [this.name, this.description, this.price, this.seller]);
+		this.id = result.insertId;
+		return this;
 	}
 
 	/**
@@ -37,7 +51,7 @@ module.exports = class Article {
 	 * @returns {Promise<void>}
 	 */
 	async update() {
-		await db.query('UPDATE articles SET name = ?, description = ?, seller_id = ? WHERE id = ?', [this.name, this.description, this.seller, this.id]);
+		await db.query('UPDATE articles SET name = ?, description = ?, price = ?, seller_id = ? WHERE id = ?', [this.name, this.description, this.price, this.seller, this.id]);
 	}
 
 	/**
--- code/Backend/server.js
+++ code/Backend/server.js
@@ -1,10 +1,11 @@
 //TODO: convert to ES modules
 require('dotenv').config();
 const express = require('express');
+
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
 	res.redirect('/api');
