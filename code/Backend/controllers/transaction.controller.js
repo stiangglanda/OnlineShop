fce85eaffe2ec817b16ce3dbb148e8af69dc757a
Leander Kieweg <Leander Kieweg@DESKTOP-IGFI88Q>
2023-05-11 14:18:13 +0200

    changed transaction because of coding conventions (ids)

--- code/Backend/controllers/transaction.controller.js
+++ code/Backend/controllers/transaction.controller.js
@@ -3,7 +3,7 @@ import Transaction from '../models/transaction.js';
 const createTransaction = async (req, res) => {
 	console.log(new Date());
 	const id = await Transaction.nextId();
-	const transaction = new Transaction(id, req.body.seller, req.body.buyer, req.body.article, new Date());
+	const transaction = new Transaction(id, req.body.seller_id, req.body.buyer_id, req.body.article_id, new Date());
 	try {
 		res.status(201).json(await transaction.save());
 	} catch (error) {
