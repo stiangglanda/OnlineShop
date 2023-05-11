e346cf73ef1cfc60e59955799393e77112c58c1a
Tim <timhofmann01@gmail.com>
2023-05-11 21:09:14 +0200

    better /api/ page

--- code/Backend/server.js
+++ code/Backend/server.js
@@ -1,8 +1,14 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import express, { json, urlencoded } from 'express';
 
 import articlesRouter from './routes/articles.js';
 import usersRouter from './routes/users.js';
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -16,6 +22,25 @@ app.get('/', function (req, res) {
 app.use('/api/articles', articlesRouter);
 app.use('/api/users', usersRouter);
 
+app.use('/api', function (req, res) {
+	// get routes from file directory
+	let routes = path.join(__dirname, 'routes');
+	routes = fs.readdirSync(routes);
+
+	// remove .js from route names
+	routes = routes.map((route) => {
+		return route.replace('.js', '');
+	});
+
+	let html = '<h1>API Routes</h1>';
+	html += '<ul>';
+	routes.forEach((route) => {
+		html += `<li><a href="/api/${route}">/api/${route}</a></li>`;
+	});
+	html += '</ul>';
+
+	res.send(html);
+});
 
 app.listen(3000, function () {
 	console.log('Backend at http://localhost:3000');
