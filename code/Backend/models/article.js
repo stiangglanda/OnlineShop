7417f845faba105e3d23b04c0444796aa9085d56
Leander Kieweg <Leander Kieweg@DESKTOP-IGFI88Q>
2023-05-12 09:22:01 +0200

    gods work wos done in the article get

--- code/Backend/models/article.js
+++ code/Backend/models/article.js
@@ -17,9 +17,39 @@ export default class Article {
 	 * @returns {Promise<Array<Article>>} The articles.
 	 */
 	static async list() {
-		const [rows] = await db.query('SELECT * FROM article WHERE status = 1');
-		return rows.map((row) => new Article(row.id, row.status, row.name, row.description, row.price, row.seller_id));
-	}
+		const [articles] = await db.query('SELECT * FROM article WHERE status = 1');
+		let [images] = await db.query('SELECT id, url, article_id FROM image');
+		images = images.map((image) => {
+			return {
+				id: image.id,
+				url: image.url,
+				article_id: image.article_id
+			};
+		});
+
+		let [categories] = await db.query('SELECT * FROM category');
+		categories = categories.map((category) => {
+			return {
+				id: category.id,
+				name: category.name
+			};
+		});
+
+		let [article_categories] = await db.query('SELECT * FROM article_category');
+		article_categories = article_categories.map((article_category) => {
+			return {
+				article_id: article_category.article_id,
+				category_id: article_category.category_id
+			};
+		});
+        
+		return articles.map((article) => {
+			const art_img = images.filter((image) => image.article_id === article.id);
+			const art_categories = article_categories.filter((article_category) => article_category.article_id === article.id).map((article_category) => article_category.category_id);
+			const cat = categories.filter((category) => art_categories.includes(category.id));
+			return new Article(article.id, article.status, article.name, article.description, article.price, article.seller_id, cat, art_img);
+		});
+	}   
 
 	/**
 	 * Finds an article by id.
