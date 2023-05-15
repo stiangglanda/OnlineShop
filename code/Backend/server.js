ed0f6bb5314c585356c93c2c94dc992c8df287db
Tim <timhofmann01@gmail.com>
2023-05-15 12:21:07 +0200

    removed /api get

--- code/Backend/server.js
+++ code/Backend/server.js
@@ -27,25 +27,6 @@ app.use('/api/users', usersRouter);
 app.use('/api/transactions', transactionsRouter);
 app.use('/api/categories', categoriesRouter);
 
-app.use('/api', function (req, res) {
-	// get routes from file directory
-	let routes = path.join(__dirname, 'routes');
-	routes = fs.readdirSync(routes);
-
-	// remove .js from route names
-	routes = routes.map((route) => {
-		return route.replace('.js', '');
-	});
-
-	let html = '<h1>API Routes</h1>';
-	html += '<ul>';
-	routes.forEach((route) => {
-		html += `<li><a href="/api/${route}">/api/${route}</a></li>`;
-	});
-	html += '</ul>';
-
-	res.send(html);
-});
 
 app.listen(3000, function () {
 	console.log('Backend at http://localhost:3000');
