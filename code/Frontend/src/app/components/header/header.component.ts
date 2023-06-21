cbe628aa148f30788da1425dcf029091e95b79c4
pazidam <damberger.patrik@gmail.com>
2023-06-21 14:34:40 +0200

    show button

--- code/Frontend/src/app/components/header/header.component.ts
+++ code/Frontend/src/app/components/header/header.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private auth: AuthService, private router: Router){}
+  constructor(private auth: AuthService, public router: Router){}
 
   public isLogedIn!: boolean; 
 
