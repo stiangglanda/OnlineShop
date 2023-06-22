2bb2dfa621882434bb78e08bc134b31d6b2550db
Leander Kieweg <Leander Kieweg@DESKTOP-IGFI88Q>
2023-06-22 08:36:09 +0200

    added route for article search

--- code/Backend/routes/articles.js
+++ code/Backend/routes/articles.js
@@ -7,6 +7,10 @@ router // /api/articles
 	.get(articleCtrl.getArticles)
 	.post(articleCtrl.createArticle);
 
+router // /api/articles
+	.route('/search/:articleName')
+	.get(articleCtrl.findByName())
+
 router // /api/articles/:id
 	.route('/:id')
 	.get(articleCtrl.getArticle)
