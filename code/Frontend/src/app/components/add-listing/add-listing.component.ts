83b447078d59f5866a878f1614bb14f8580782f2
Natoteo <Lukasnatotea@gmail.com>
2023-06-28 16:02:12 +0200

    Added Add-listing

--- code/Frontend/src/app/components/add-listing/add-listing.component.ts
+++ code/Frontend/src/app/components/add-listing/add-listing.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { user_update } from 'src/app/models/user_update';
 
 @Component({
   selector: 'app-add-listing',
   templateUrl: './add-listing.component.html',
   styleUrls: ['./add-listing.component.css']
 })
-export class AddListingComponent {
+export class AddListingComponent implements OnInit {
 
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private auth: AuthService,
+    private router: Router
+  ){}
+
+  public model!: user_update;
+  private updateModel!: user_update;
+  private usernameFromToken: string = this.auth.getUsernameFromToken();
+
+  userForm!: FormGroup;   
+
+  ngOnInit(): void {
+    if(this.auth.isLoggedIn())
+    {
+      this.userService.getUserByName(this.usernameFromToken).subscribe({
+        next: (res => {
+          this.model = {
+            username: res.username,
+            firstname: res.firstname,
+            lastname: res.lastname,
+            email: res.email,
+            city: res.address.city,
+            plz: res.address.plz,
+            street: res.address.street,
+            street_nr: res.address.street_nr
+          };
+        }),
+        error: (err => { console.log(err) })
+      });
+    }else
+    {
+      this.router.navigate( ['login'] );
+    }
+
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: ['', Validators.required]
+    });
+  }
 }
